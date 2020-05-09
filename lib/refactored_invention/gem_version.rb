53324802ba440dc757ece9cfc732a889108462cc
Andrew Mason <andrewmcodes@protonmail.com>
2020-05-09 02:34:09 -0400

    Preparing for 1.0.0.rc6 release

--- lib/refactored_invention/gem_version.rb
+++ lib/refactored_invention/gem_version.rb
@@ -10,7 +10,7 @@ module RefactoredInvention # :nodoc:
     MAJOR = 1
     MINOR = 0
     TINY  = 0
-    PRE   = "rc4"
+    PRE   = "rc6"
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join(".")
   end
