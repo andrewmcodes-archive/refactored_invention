36fa1a094ac9ba9c6faba508d5ed2e427c290e29
Andrew Mason <andrewmcodes@protonmail.com>
2020-05-09 02:09:15 -0400

    bump

--- lib/refactored_invention/gem_version.rb
+++ lib/refactored_invention/gem_version.rb
@@ -10,7 +10,7 @@ module RefactoredInvention # :nodoc:
     MAJOR = 1
     MINOR = 0
     TINY  = 0
-    PRE   = "delta"
+    PRE   = "rc1"
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join(".")
   end
